d10a997dd1ab320cf91ac4f168763ec03f13d95a
SHANICA_DENIS <shanicanikad@gmail.com>
2021-09-02 01:59:41 -0400

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ const IndexPage = () => {
       {/* <Header /> */}
         <h1>Hello,</h1>
         <h2>I'm Shanica, a full-stack developer!</h2>
-        <p>Need a developer? <Link to= "/contact">Contact me.</Link></p>
       {/* <Footer /> */}
     </Layout>
   )
